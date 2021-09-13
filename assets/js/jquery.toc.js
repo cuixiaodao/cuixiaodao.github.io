2fcb692f64c46502ba729e9337475124f6496810
cuixiaodao <cuixiaodao@outlook.com>
2021-09-13 16:17:38 +0000

    Deploying to built from  @ 534538222e1d19308bf80bb1352f0271cb330e8f 🚀

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -1,5 +1,5 @@
 // https://github.com/ghiculescu/jekyll-table-of-contents
-// Updated by https://cuixiaodao.github.io
+// Updated by https://mazhuang.org
 (function($){
   $.fn.toc = function(options) {
     var defaults = {
